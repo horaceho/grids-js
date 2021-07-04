e62d243ad20bd3f0f6f66eab8ba21efdc3d20d65
Horace Ho <horaceh@gmail.com>
2021-07-04 11:51:04 +0800

    add grids vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Hello msg="Hello" />
+  <Grids title="Grids" />
 </template>
 
 <script setup>
 import Hello from './components/Hello.vue'
+import Grids from './components/Grids.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
---
+++ src/components/Grids.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>{{ title }}</h1>
+    <p>{{ rows }} x {{ cols }} in {{ total }} grids</p>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+
+export default {
+    props: {
+        title: String,
+        rows: {
+            type: Number,
+            default: 10,
+        },
+        cols: {
+            type: Number,
+            default: 9,
+        },
+    },
+    mounted() {
+        console.log('grids mounted');
+    },
+    unmounted() {
+        console.log('grids unmounted()');
+    },
+    setup(props) {
+        const total = props.rows * props.cols;
+        return {
+            total,
+        };
+    },
+};
+</script>
